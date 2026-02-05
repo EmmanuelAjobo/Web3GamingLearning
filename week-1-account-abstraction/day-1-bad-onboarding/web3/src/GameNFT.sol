ddafbe95d08f62cf2f87824060682f5f1c7837e1
Luxon Protocol <142466199+EmmanuelAjobo@users.noreply.github.com>
2026-02-05 22:07:42 +0100

    day0

--- week-1-account-abstraction/day-1-bad-onboarding/web3/src/GameNFT.sol
+++ week-1-account-abstraction/day-1-bad-onboarding/web3/src/GameNFT.sol
@@ -2,12 +2,36 @@
 pragma solidity ^0.8.20;
 contract Game {
 
-    uint256 private healthbar = 10;
+    mapping (address => bool) public hasSword;
+    mapping (address => uint256) public swordStrength;
 
-    function fight() public returns (uint256){
+    uint256 private claimNumber;
 
-        healthbar++;
-        return healthbar;
+    event SwordClaimed(address indexed player, uint256 power);
+    event SwordUpgraded(address indexed player, uint256 newPower);
+
+
+    function claimSword() public {
+
+        require(!hasSword[msg.sender], "Already has sword, theif");
+
+        hasSword[msg.sender] = true;
+        claimNumber++;
+        swordStrength[msg.sender] = 1;
+
+        emit SwordClaimed(msg.sender, swordStrength[msg.sender]);
+        
+    }
+
+    function upgradeSword() public {
+        require (hasSword[msg.sender], "Can't upgrade what you dont have");
+        swordStrength[msg.sender]++;
+
+       emit SwordUpgraded(msg.sender, swordStrength[msg.sender]);
+    }
+
+    function checkPlayerStats() public view returns (bool, uint256){
+        return(hasSword[msg.sender], swordStrength[msg.sender]);
     }
 
 }
